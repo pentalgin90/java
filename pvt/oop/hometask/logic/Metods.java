e892a19bc20e11b157144340323a6ee52e8b18aa
Dmitriy <32781137+pentalgin90@users.noreply.github.com>
2017-11-05 16:13:58 +0300

    Add files via upload

--- pvt/oop/hometask/logic/Metods.java
+++ pvt/oop/hometask/logic/Metods.java
@@ -4,30 +4,68 @@ import java.util.Scanner;
 
 import by.pvt.oop.hometask.bin.Student;
 
-public class  Metods{
-	
+public class Metods {
+
 	public int intZnach(String name) {
 		System.out.println(name);
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		return a;
 	}
-	
+
 	public String stringZnach(String pole) {
 		System.out.println(pole);
 		Scanner sp = new Scanner(System.in);
 		String b = sp.nextLine();
 		return b;
 	}
-	public boolean sravnPoley(Student a, Student proverka) {
-		boolean log = false;
-		int result = a.getFakultet().compareTo(proverka.getFakultet());
-		if(result == 0) {
-			log = true;
-		}else {
-			log = false;
+
+	/*
+	 * public boolean proverkaPolya(Student c, Student arr) { boolean rezult =
+	 * false; if(arr.getFakultet().equals(c.getFakultet())) { rezult = true;
+	 * 
+	 * }else { rezult = false;
+	 * 
+	 * }
+	 * 
+	 * return rezult; }
+	 */
+
+	public void proverkaPolya(String c, Student[] arr) { //сравнивает поле факультет
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i].getFakultet().equals(c)) {
+				System.out.println(arr[i]);
+			}
 		}
-		return log;
 	}
+	public void proverkaKurs(String c, Student[] arr) { // сравнивает поле kurs
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].getKurs().equals(c)) {
+				System.out.println(arr[i]);
+			}
+		}
+
+	}
+
+	public void proverkaInt(int c, Student[] arr) { // сравнивает дады рождения
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].getDate() > c) {
+				System.out.println(arr[i]);
+			}
+		}
+
+	}
+	
+	
+	public void proverkaGroup(String c, Student[] arr) { // сравнение поля group
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].getGroup().equals(c)) {
+				System.out.println(arr[i]);
+			}
+		}
+
+	}
+	
+	
 	
 }
